000f454f1aaca97b256f88ad131fd56a50e2a684
Alsaef <saef.ratul@gmail.com>
2025-06-19 01:00:54 +0600

    development code change

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const cors = require('cors')
-var admin = require("firebase-admin");
-var serviceAccount = require("./firebaseAdminKey.json.json");
 
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(cors())
 
-admin.initializeApp({
-
-  credential: admin.credential.cert(serviceAccount)
-
-});
 
 
-const verifyFirebaseToken = async (req, res, next) => {
+const verifToken = async (req, res, next) => {
   const authHeader = req.headers?.authorization;
+  console.log(authHeader);
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).send({ message: 'unauthorized access' })
   }
@@ -26,7 +21,8 @@ const verifyFirebaseToken = async (req, res, next) => {
   const token = authHeader.split(' ')[1];
 
   try {
-    const decoded = await admin.auth().verifyIdToken(token)
+    const decoded =  jwt.verify(token,`0689be11d6dfdc71455635308d90d590315729da4549b1a91fa35093c760143bbd07f564d81ee929719dab0a0ddc58bae21a065adfdf1d3d9fd1
+4928947eaff3`)
     req.decoded = decoded;
     next();
   } catch (error) {
@@ -70,18 +66,34 @@ async function run() {
       if (existingUser) {
         return res.status(409).send({ message: 'User already exists' });
       }
-      const newUser = { name, photoUrl, email,role:'user' };
+      const newUser = { name, photoUrl, email, role: 'user' };
       const result = await usersCallections.insertOne(newUser);
       res.send(result);
     });
-   
 
-      app.get('/api/v1/users/admin/:email',verifyFirebaseToken,async(req,res)=>{
+
+    app.get('/api/v1/users/admin/:email',verifToken, async (req, res) => {
       const email = req.params.email;
-      const query={email: email}
-      const user= await usersCallections.findOne(query)
-      const result={admin: user?.role==='admin'}
+      const query = { email: email }
+      const user = await usersCallections.findOne(query)
+      const result = { admin: user?.role === 'admin' }
       res.send(result)
+    })
+
+
+    app.get('/api/v1/users', async (req, res) => {
+      const result = await usersCallections.find({}).toArray()
+      res.status(200).send(result)
+    })
+
+
+    app.post('/api/v1/jwt', async (req, res) => {
+      const { email } = req.body
+      const token = jwt.sign({ email: email }, `0689be11d6dfdc71455635308d90d590315729da4549b1a91fa35093c760143bbd07f564d81ee929719dab0a0ddc58bae21a065adfdf1d3d9fd1
+4928947eaff3`, { expiresIn: '7d' })
+
+      res.status(200).send(token)
+      
     })
 
 
